3e927c75180b2f5a51d55c7fa60f3e1a2c52f06c
Gökhan Çam <76878009+gokcam@users.noreply.github.com>
2022-01-31 19:25:09 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+            while(sayac<=sayi)
+            {
+                toplam += sayac;
+                sayac++;
+            }
+            Console.WriteLine(toplam);
+
+
+            // 'a' dan 'z' ye kadar tüm harfleri yazdır
+            char character = 'a';
+            while(character < 'z')
+            {
+                Console.WriteLine(character);
+                character++;
+            }
+
+            //Foreach
+            Console.WriteLine("*******Foreach********");
+            string[] cars = { "bmw", "toyota", "nissan" };
+            foreach (var item in cars)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
